2f3556a3d147855c1938d84ec344b21324a726a6
Alessandro D"Amico <alessandro.damico@sorint.it>
2017-01-09 18:05:30 +0100

    Posizione

--- www/js/app/controllers/secondctrl/secondCtrl.js
+++ www/js/app/controllers/secondctrl/secondCtrl.js
@@ -1,6 +1,26 @@
 angular.module('myApp').controller('secondCtrl',['$scope', '$location', function ($scope, $location) {
+  $scope.cordinate = null;
+
   $scope.backPage = function() {
     //cambio pagina
 		$location.path("/");
+  }
+
+  $scope.getPosizione = function() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $scope.cordinate = 'Latitude: '+ position.coords.latitude +
+              ' Longitude: '+ position.coords.longitude +
+              ' Altitude: ' + position.coords.altitude +
+              ' Accuracy: ' + position.coords.accuracy +
+              ' Altitude Accuracy: ' + position.coords.altitudeAccuracy +
+              ' Heading: ' + position.coords.heading +
+              ' Speed: ' + position.coords.speed +
+              ' Timestamp: ' + position.timestamp;
+
+      $scope.$apply();
+
+    }, function(error) {
+      alert('code: '+ error.code + 'message: ' + error.message);
+    });
   }
 }]);
